c5b2fe57b504fddb90f8ced1132979d209f0eac1
Aquib Shahbaz <justaquib@Aquibs-MacBook-Air.local>
2025-07-02 14:04:07 +0530

    fix next config

--- next.config.ts
+++ next.config.ts
@@ -1,22 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async redirects() {
-    return [
-      {
-        source: "/(.*)",
-        has: [
-          {
-            type: "header",
-            key: "x-forwarded-proto",
-            value: "http",
-          },
-        ],
-        destination: "http://www.justaquib.com/:path*",
-        permanent: false,
-      },
-    ];
-  },
+  /* config options here */
 };
 
 export default nextConfig;
